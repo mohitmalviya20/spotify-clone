5187479c7ebe214b515d26f227fcd5ab168a70a8
mohit malviya <56771492+mohitmalviya20@users.noreply.github.com>
2020-10-16 00:11:35 +0530

    Update Header.js
    added signout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
-import { Avatar } from "@material-ui/core";
-import { SearchOutlined } from "@material-ui/icons";
+import { Avatar, IconButton } from "@material-ui/core";
+import { ExitToApp, SearchOutlined } from "@material-ui/icons";
 import React from "react";
 import { useDataLayerValue } from "../DataLayer";
 import "./css/Header.css";
 function Header({ spotify }) {
-  const [{ user }] = useDataLayerValue();
+  const [{ user },dispatch] = useDataLayerValue();
+  const signout=()=>{
+    dispatch({
+      type: "SET_TOKEN",
+      token: null,
+    })
+  }
   return (
     <div className="header">
       <div className="header_left">
@@ -17,6 +23,9 @@ function Header({ spotify }) {
       <div className="header_right">
         <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
         <h4>{user?.display_name}</h4>
+        <IconButton className="signout" onClick={signout}>
+        <ExitToApp/>
+        </IconButton>
       </div>
     </div>
   );
